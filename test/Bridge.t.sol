bde923a54dce55799c356f4b47be5a85b5d7859f
Confucian <confucian.e@gmail.com>
2023-08-08 17:25:36 +0800

    feat: finish Bridge test

---
+++ test/Bridge.t.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "forge-std/Test.sol";
+import "../src/Bridge.sol";
+import "../src/TestToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract BridgeTest is Test {
+    event BridgeIn(
+        address indexed operator,
+        address indexed token,
+        uint256 amount
+    );
+
+    event BridgeOut(
+        address indexed receiver,
+        address indexed token,
+        uint256 amount
+    );
+
+    TestToken public tt;
+    Bridge public bridge;
+
+    function setUp() public {
+        bridge = new Bridge();
+        tt = new TestToken();
+
+        deal(address(tt), address(this), 1e26);
+        IERC20(tt).approve(address(bridge), 1e26);
+    }
+
+    function testSupportNewToken() public {
+        assertEq(bridge.isSupported(address(tt)), false);
+        bridge.supportNewToken(address(tt));
+        assertEq(bridge.isSupported(address(tt)), true);
+    }
+
+    function testAddReserve() public {
+        bridge.supportNewToken(address(tt));
+        assertEq(bridge.reserve(address(tt)), 0);
+        bridge.addReserve(address(tt), 100 ether);
+        assertEq(bridge.reserve(address(tt)), 100 ether);
+    }
+
+    function testBridgeIn() public {
+        bridge.supportNewToken(address(tt));
+        bridge.addReserve(address(tt), 100 ether);
+
+        vm.expectEmit();
+        emit BridgeIn(address(this), address(tt), 10 ether);
+
+        bridge.bridgeIn(address(tt), 10 ether, address(this));
+        assertEq(bridge.reserve(address(tt)), 90 ether);
+    }
+
+    function testBridgeOut() public {
+        bridge.supportNewToken(address(tt));
+        bridge.addReserve(address(tt), 100 ether);
+
+        vm.expectEmit();
+        emit BridgeOut(address(1), address(tt), 1 ether);
+
+        bridge.bridgeOut(address(tt), 1 ether, address(1));
+        assertEq(bridge.reserve(address(tt)), 99 ether);
+    }
+}
